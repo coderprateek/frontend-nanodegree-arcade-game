1875ea3f6415af87a336b9c8c0f463c84beb0359
Prateek Roushan <prateekroushan11@gmail.com>
2017-08-16 22:40:19 +0530

    intial

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
+    // we've provided one for you to get s
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
